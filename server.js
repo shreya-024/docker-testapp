1fcb7851e2d6f6ffb05bf1255468a834b3cae198
shreya-024 <shreyak.2406@gmail.com.com>
2025-08-07 20:06:23 +0530

    Initial commit

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL = "mongodb://admin:qwerty@mongo:27017";
 const client = new MongoClient(MONGO_URL);
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
+    await client.connect();
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
+    const db = client.db("my-db");
     const data = await db.collection('users').find({}).toArray();
     
     client.close();
@@ -26,14 +26,16 @@ app.get("/getUsers", async (req, res) => {
 app.post("/addUser", async (req, res) => {
     const userObj = req.body;
     console.log(req.body);
-    await client.connect(URL);
+    await client.connect();
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
+    const db = client.db("my-db");
     const data = await db.collection('users').insertOne(userObj);
     console.log(data);
     console.log("data inserted in DB");
     client.close();
+
+    res.redirect('http://localhost:8081');
 });
 
 
